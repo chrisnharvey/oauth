730133c21e3e0baba94ef1546ea5038b1f62d655
Chris Harvey <chris@chrisnharvey.com>
2013-02-08 22:19:51 +0000

    Moving Authorize token class into OAuth2 namespace

--- src/OAuth/OAuth2/Token/Authorize.php
+++ src/OAuth/OAuth2/Token/Authorize.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace OAuth\Token;
+namespace OAuth\OAuth2\Token;
 /**
  * OAuth2 Token
  *
@@ -10,7 +10,7 @@ namespace OAuth\Token;
  * @copyright  (c) 2011 HappyNinjas Ltd
  */
 
-class Authorize extends \OAuth\Token
+class Authorize extends \OAuth\OAuth2\Token
 {
 	/**
 	 * @var  string  code
