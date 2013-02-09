4f302f76240f17c8c478eec48517f0237e84f9c7
Chris Harvey <chris@chrisnharvey.com>
2013-02-09 11:08:46 +0000

    Moving Twitter provider and formatting for PSR-2

---
+++ src/OAuth/Provider/Twitter.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace OAuth\Provider;
+
+use \OAuth\OAuth1\Token\Access;
+use \OAuth\OAuth1\Consumer;
+
+class Twitter extends \OAuth\OAuth1\Provider
+{
+
+    public $name = 'twitter';
+    
+    public $uid_key = 'user_id';
+
+    public function requestTokenUrl()
+    {
+        return 'https://api.twitter.com/oauth/request_token';
+    }
+
+    public function authorizeUrl()
+    {
+        return 'https://api.twitter.com/oauth/authorize';
+    }
+
+    public function accessTokenUrl()
+    {
+        return 'https://api.twitter.com/oauth/access_token';
+    }
+    
+    public function getUserInfo(Consumer $consumer, Access $token)
+    {       
+        // Create a new GET request with the required parameters
+        $request = Consumer::forge('resource', 'GET', 'http://api.twitter.com/1/users/lookup.json', array(
+            'oauth_consumer_key' => $consumer->key,
+            'oauth_token' => $token->access_token,
+            'user_id' => $token->uid,
+        ));
+
+        // Sign the request using the consumer and token
+        $request->sign($this->signature, $consumer, $token);
+
+        $user = current(json_decode($request->execute()));
+        
+        // Create a response from the request
+        return array(
+            'uid' => $token->uid,
+            'nickname' => $user->screen_name,
+            'name' => $user->name ? $user->name : $user->screen_name,
+            'location' => $user->location,
+            'image' => $user->profile_image_url,
+            'description' => $user->description,
+            'urls' => array(
+              'Website' => $user->url,
+              'Twitter' => 'http://twitter.com/'.$user->screen_name,
+            ),
+        );
+    }
+
+} // End Provider_Twitter
